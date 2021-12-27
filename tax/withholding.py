f78834da3c6bc6834556fba7acb41e01f8e94006
finspresso <coder@finspresso.com>
2021-12-27 17:21:27 +0100

    Beautification

--- tax/withholding.py
+++ tax/withholding.py
@@ -12,10 +12,15 @@ import pyqtgraph as pg
 import sys
 from pyqtgraph.Qt import QtGui
 
+
+# Global settings
 coloredlogs.install()
 logger_name = __name__ if __name__ != "__main__" else "witholding"
 logger = logging.getLogger(logger_name)
 logging.basicConfig(level=logging.DEBUG)
+
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
 
 
 class TaxWithholder:
@@ -36,12 +41,12 @@ class TaxWithholder:
         self.load_steuerfuss()
         self.compute_canton_taxes()
         self.plotting_app = PlottingApp(
-            combo_list=self.steuerfuss_dict,
+            combo_list=[*self.steuerfuss_dict],
             update_function=self.update_municipality_input,
+            selected_combobox_list=self.municipality,
         )
         self.update_plot_canton_taxes()
         self.update_municipality_input(self.municipality)
-        # import pdb; pdb.set_trace()
 
     def load_tax_rates(self):
         self.tax_rates_dict = dict()
@@ -79,20 +84,21 @@ class TaxWithholder:
         )
 
     def update_plot_canton_taxes(self):
+        pen = pg.mkPen(color="g", width=4)
         if self.plot_canton_taxes is None:
             self.plot_canton_taxes = self.plotting_app.plot_widget.plot(
-                self.incomes_samples, self.taxes_canton, name="Tax Canton"
+                self.incomes_samples, self.taxes_canton, name="Tax canton", pen=pen
             )
         else:
             self.plot_canton_taxes.setData(self.incomes_samples, self.taxes_canton)
 
     def update_plot_municipality_taxes(self):
-        pen = pg.mkPen(color=(200, 200, 255), width=1)
+        pen = pg.mkPen(color="r", width=4)
         if self.plot_municipality_taxes is None:
             self.plot_municipality_taxes = self.plotting_app.plot_widget.plot(
                 self.incomes_samples,
-                self.taxes_canton,
-                name="Municipality tax",
+                self.taxes_municipality,
+                name="Tax municipality",
                 pen=pen,
             )
         else:
@@ -109,13 +115,18 @@ class TaxWithholder:
 
 
 class PlottingApp(QtGui.QWidget):
-    def __init__(self, combo_list=[], update_function=None):
+    def __init__(
+        self, combo_list=[], selected_combobox_list=None, update_function=None
+    ):
         QtGui.QWidget.__init__(self)
         self.setWindowTitle("Taxes")
         self.main_layout = QtGui.QVBoxLayout()
         self.municipality_cb = QtGui.QComboBox()
         if combo_list:
+            combo_list.sort()
             self.municipality_cb.addItems(combo_list)
+        if selected_combobox_list is not None:
+            self.municipality_cb.setCurrentText(selected_combobox_list)
 
         if update_function is not None:
             self.municipality_cb.currentTextChanged.connect(update_function)
@@ -126,80 +137,13 @@ class PlottingApp(QtGui.QWidget):
         self.setLayout(self.main_layout)
         self.plot_widget = pg.PlotWidget()
         self.plot_widget.addLegend()
-        self.plot_widget.setBackground((255, 255, 255))
+        self.plot_widget.showGrid(x=True, y=True, alpha=0.4)
+        self.plot_widget.setLabel("bottom", "taxable income [CHF]")
+        self.plot_widget.setLabel("left", "tax [CHF]")
         self.main_layout.addWidget(self.plot_widget)
 
     def update_data(self, x, y):
         self.plot_widget.plot(x, y)
-
-    # def municipality_cb_changed(self):
-    #    print(str(self.municipality_cb.currentText()))
-
-    #     self.central_layout = QtGui.QVBoxLayout()
-    #     self.plot_boxes_layout = QtGui.QHBoxLayout()
-    #     self.boxes_layout = QtGui.QVBoxLayout()
-    #     self.setLayout(self.central_layout)
-
-    #     # Lets create some widgets inside
-    #     self.label = QtGui.QLabel('Taxes plots')
-
-    #     # Here is the plot widget from pyqtgraph
-    #     self.plot_widget = pg.PlotWidget()
-
-    #     # Now the Check Boxes (lets make 3 of them)
-    #     self.num = 6
-    #     self.check_boxes = [QtGui.QCheckBox(f"Box {i+1}") for i in range(self.num)]
-
-    #     # Here will be the data of the plot
-    #     self.plot_data = [None for _ in range(self.num)]
-
-    #     # Now we build the entire GUI
-    #     self.central_layout.addWidget(self.label)
-    #     self.central_layout.addLayout(self.plot_boxes_layout)
-    #     self.plot_boxes_layout.addWidget(self.plot_widget)
-    #     self.plot_boxes_layout.addLayout(self.boxes_layout)
-    #     for i in range(self.num):
-    #         self.boxes_layout.addWidget(self.check_boxes[i])
-    #         # This will conect each box to the same action
-    #         self.check_boxes[i].stateChanged.connect(self.box_changed)
-
-    #     # For optimization let's create a list with the states of the boxes
-    #     self.state = [False for _ in range(self.num)]
-
-    #     # Make a list to save the data of each box
-    #     self.box_data = [[[0], [0]] for _ in range(self.num)]
-    #     x = np.linspace(0, 3.14, 100)
-    #     self.add_data(x, np.sin(x), 0)
-    #     self.add_data(x, np.cos(x), 1)
-    #     self.add_data(x, np.sin(x)+np.cos(x), 2)
-    #     self.add_data(x, np.sin(x)**2, 3)
-    #     self.add_data(x, np.cos(x)**2, 4)
-    #     self.add_data(x, x*0.2, 5)
-
-    # def add_data(self, x, y, ind):
-    #     self.box_data[ind] = [x, y]
-    #     if self.plot_data[ind] is not None:
-    #         self.plot_data[ind].setData(x, y)
-
-    # def box_changed(self):
-    #     for i in range(self.num):
-    #         if self.check_boxes[i].isChecked() != self.state[i]:
-    #             self.state[i] = self.check_boxes[i].isChecked()
-    #             if self.state[i]:
-    #                 if self.plot_data[i] is not None:
-    #                     self.plot_widget.addItem(self.plot_data[i])
-    #                 else:
-    #                     self.plot_data[i] = self.plot_widget.plot(*self.box_data[i])
-    #             else:
-    #                 self.plot_widget.removeItem(self.plot_data[i])
-    #             break
-
-
-# if __name__ == "__main__":
-#     app = QtGui.QApplication(sys.argv)
-#     window = MyApp()
-#     window.show()
-#     sys.exit(app.exec_())
 
 
 def main():
@@ -230,9 +174,6 @@ def main():
     )
     tax_withholder.plotting_app.show()
     sys.exit(app.exec_())
-
-    #
-    # pg.QtGui.QApplication.exec_()
 
 
 if __name__ == "__main__":
