3b9c7532a83435bfcb7e6f5c02602a80486749cc
finspresso <coder@finspresso.com>
2022-01-07 08:54:14 +0100

    Initial commit portfolio_tracker

---
+++ portfolio_tracker/portfolio_tracker.py
@@ -0,0 +1,16 @@
+# Next load holdings and compute projected dividends
+class DividendProjector:
+    def __init__(self):
+        print("Hello")
+
+    def print(self):
+        print("Hello")
+
+
+def main():
+    dividend_projector = DividendProjector()
+    dividend_projector.print()
+
+
+if __name__ == "__main__":
+    main()
