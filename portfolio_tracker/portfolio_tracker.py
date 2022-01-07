1a5ac9af5aa7c74b4e5a1333ee29f7c9d2122982
finspresso <coder@finspresso.com>
2022-01-07 09:01:55 +0100

    Load holdings

--- portfolio_tracker/portfolio_tracker.py
+++ portfolio_tracker/portfolio_tracker.py
@@ -1,14 +1,33 @@
 # Next load holdings and compute projected dividends
+import argparse
+import json
+
+
+from pathlib import Path
+
+
 class DividendProjector:
-    def __init__(self):
-        print("Hello")
+    def __init__(self, holdings_file="holdings.json"):
+        self.holdings_file = Path(holdings_file)
+        self.load_holdings()
+
+    def load_holdings(self):
+        with self.holdings_file.open("r") as file:
+            self.holdings_dict = json.load(file)
 
     def print(self):
-        print("Hello")
+        print(self.holdings_dict)
 
 
 def main():
-    dividend_projector = DividendProjector()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--holdings_file",
+        help="JSON file containing portfolio holdings",
+        default="holdings.json",
+    )
+    args = parser.parse_args()
+    dividend_projector = DividendProjector(holdings_file=args.holdings_file)
     dividend_projector.print()
 
 
