d0672b0125d491a8f33f7fd542c199d5fbfeb4c7
finspresso <coder@finspresso.com>
2022-03-24 15:33:17 +0100

    Load tickers from file

--- quant/dividend_analyzer.py
+++ quant/dividend_analyzer.py
@@ -12,11 +12,14 @@ coloredlogs.install()
 logger_name = "dividend_analyzer"
 logger = logging.getLogger(logger_name)
 logging.basicConfig(level=logging.DEBUG)
+SP_500_TICKER_FILE = "sp_500_tickers.json"
 
 
 class DividendAnalyzer:
-    def __init__(self):
-        pass
+    def __init__(self, tickers, n_security=10, n_portfolio=100):
+        self.tickers = tickers
+        self.n_security = n_security
+        self.n_portfolio = n_portfolio
 
     @classmethod
     def get_SP500_constituents(self):
@@ -31,25 +34,49 @@ class DividendAnalyzer:
 
     @classmethod
     def store_constituents_to_json(self, constituents_dict, file_name):
-        logger.info("Dumping constituents ticker dictionary to %s", file_name)
+        logger.info("Dumping constituents' ticker dictionary to %s", file_name)
 
         with open(file_name, "w") as file:
             json.dump(constituents_dict, file, indent=4)
+
+    @classmethod
+    def load_constituents_from_json(self, file_name):
+        logger.info("Loading constituents' ticker from %s", file_name)
+
+        with open(file_name, "r") as file:
+            constituents_dict = json.load(file)
+        return constituents_dict
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--download_sp_500",
-        help="If set downloads the consitutents of the S&P 500 index from Wikipedia under sp_500_tickers.json",
+        help="If set downloads the consitutents of the S&P 500 index from Wikipedia under "
+        + SP_500_TICKER_FILE,
         action="store_true",
+    )
+    parser.add_argument(
+        "--n_security", help="Number of securities per portfolio", type=int, default=10
+    )
+    parser.add_argument(
+        "--n_portfolio",
+        help="Number of considered portfolios for analysis",
+        type=int,
+        default=100,
     )
     args = parser.parse_args()
     if args.download_sp_500:
         sp_500_tickers = DividendAnalyzer.get_SP500_constituents()
-        DividendAnalyzer.store_constituents_to_json(
-            sp_500_tickers, "sp_500_tickers.json"
+        DividendAnalyzer.store_constituents_to_json(sp_500_tickers, SP_500_TICKER_FILE)
+    else:
+        sp_500_tickers = DividendAnalyzer.load_constituents_from_json(
+            SP_500_TICKER_FILE
         )
+
+    DividendAnalyzer(
+        sp_500_tickers, n_security=args.n_security, n_portfolio=args.n_portfolio
+    )
 
 
 if __name__ == "__main__":
