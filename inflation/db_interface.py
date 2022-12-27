b0740668a1ec29ac17cd7893001e85598cb41e18
finspresso <coder@finspresso.com>
2022-12-27 09:41:55 +0100

    Allow creation of test table

--- inflation/db_interface.py
+++ inflation/db_interface.py
@@ -1,6 +1,8 @@
+import argparse
 import json
-import sqlalchemy
 import logging.config
+
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 
 logger = logging.getLogger("db_interace")
 # logger.setLevel(logging.DEBUG)
@@ -8,13 +10,11 @@ logger = logging.getLogger("db_interace")
 
 class DBInterface:
     def __init__(self):
-        self.engine = sqlalchemy.create_engine(
-            "mysql://root:@127.0.0.1/dummy", echo=True
-        )
-        print(self.engine.table_names())
+        self.engine = create_engine("mysql://root:@127.0.0.1/dummy", echo=True)
+        # print(self.engine.table_names())
 
-    def create_table(self):
-        pass
+    def create_table(self, meta):
+        meta.create_all(self.engine)
 
 
 def setup_logger(config_path):
@@ -24,10 +24,27 @@ def setup_logger(config_path):
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--create_test_table", action="store_true")
+    args = parser.parse_args()
     setup_logger("logging_config.json")
     logger.info("test")
     db_interface = DBInterface()
-    db_interface.create_table()
+    if args.create_test_table:
+        create_test_table(db_interface)
+
+
+def create_test_table(db_interface):
+    logger.info("Creating test table")
+    meta = MetaData()
+    Table(
+        "test_table",
+        meta,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(12)),
+        Column("lastname", String(12)),
+    )
+    db_interface.create_table(meta)
 
 
 if __name__ == "__main__":
