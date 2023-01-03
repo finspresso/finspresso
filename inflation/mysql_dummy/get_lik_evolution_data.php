5880c2ca14c1b8b917c3e8347338f58064f888a2
finspresso <coder@finspresso.com>
2023-01-03 09:50:31 +0100

    Retrieving LIK evolution data

---
+++ inflation/mysql_dummy/get_lik_evolution_data.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: application/json');
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'dummy');
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//$mysqli = new mysqli("servername", "username", "password", "dbname");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$desired_category = $_GET['q'];
+//$desired_category = '`Living and energy`';
+$query = sprintf("SELECT $desired_category, Date FROM lik_evolution ORDER BY Date");
+$result = $mysqli->query($query);
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+//echo "This is data.php"
+//print("Hello world!");
+print json_encode($data);
+?>
