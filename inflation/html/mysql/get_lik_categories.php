8776641e220d44a49e4757a67083ee9a0d2b32bd
finspresso <coder@finspresso.com>
2023-01-05 09:46:52 +0100

    Load colors from SQL table

---
+++ inflation/html/mysql/get_lik_categories.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-Type: application/json');
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'dummy');
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "SHOW COLUMNS FROM `lik_evolution`";
+$result = $mysqli->query($sql);
+// while($row = mysqli_fetch_array($result)){
+//     echo $row['Field']."<br>";
+// }
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+print json_encode($data);
+?>
