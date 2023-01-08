4a510a1363f2051ca3a02c759054ee9b3310c67a
finspresso <coder@finspresso.com>
2023-01-08 15:48:21 +0100

    Make it work on finspresso.com

--- inflation/html/mysql/db_finspresso/get_lik_colors.php
+++ inflation/html/mysql/db_finspresso/get_lik_colors.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once "db_credentials_local.php";
+require_once "db_credentials.php";
 $mysqli = new mysqli($DBhostname, $DBusername, $DBpassword, $DBname, $DBPort);
 if($mysqli->connect_error) {
   exit('Could not connect');
