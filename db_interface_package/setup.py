e517781d066a23ecf83ff10224a2fe1036eab957
finspresso <coder@finspresso.com>
2023-05-19 15:44:19 +0200

    Add db_interface_package

---
+++ db_interface_package/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="db_interface",
+    version="0.0.1",
+    author="Jon Lemonade",
+    description="Interface to sqlalchemy ORM",
+    packages=["db_interface"],
+)
