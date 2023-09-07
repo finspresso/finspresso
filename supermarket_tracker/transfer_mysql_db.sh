926849c089299688dca30487fdce0403727a6da5
finspresso <coder@finspresso.com>
2023-09-07 21:57:20 +0200

    Add correct db name

--- supermarket_tracker/transfer_mysql_db.sh
+++ supermarket_tracker/transfer_mysql_db.sh
@@ -8,4 +8,4 @@ mysqldump="/opt/lampp/bin/mysqldump"
 mysql="/opt/lampp/bin/mysql"
 
 $mysqldump -u root -p supermarket > dump.sql
-$mysql -u root -p --port 3308 --host 127.0.0.1 < dump.sql
+$mysql -u root -p --port 3308 --host 127.0.0.1 supermarket < dump.sql
