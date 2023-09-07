0f5aee4125f6cc51e92edb2dff39c8fea234ea75
finspresso <coder@finspresso.com>
2023-09-07 14:29:15 +0200

    Simpler transfer

--- supermarket_tracker/transfer_mysql_db.sh
+++ supermarket_tracker/transfer_mysql_db.sh
@@ -7,5 +7,5 @@ source $HOME/.bash_aliases
 mysqldump="/opt/lampp/bin/mysqldump"
 mysql="/opt/lampp/bin/mysql"
 
-$mysqldump -u root -p --all-databases --ignore-table=mysql.innodb_index_stats --ignore-table=mysql.innodb_table_stats  --ignore-table=mysql.table_stats --ignore-table=mysql.index_stats > dump.sql
+$mysqldump -u root -p supermarket > dump.sql
 $mysql -u root -p --port 3308 --host 127.0.0.1 < dump.sql
