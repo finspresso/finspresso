1dea055444adb1804ca47db5a83bc7b992f0f50b
finspresso <coder@finspresso.com>
2023-10-29 15:40:29 +0100

    Add button press to start/stop process

--- bialetti/arduino/stepper_actuation/stepper_actuation/stepper_actuation.ino
+++ bialetti/arduino/stepper_actuation/stepper_actuation/stepper_actuation.ino
@@ -2,17 +2,35 @@
 
 const int stepsPerRevolution = 2048; // change this to fit the number of steps per revolution
 const int rolePerMinute = 10;        // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
+int currentState = -1;
 
 //set steps and the connection with MCU
 Stepper stepper(stepsPerRevolution, 2, 3, 4, 5);
 bool clockwise = false;
 void setup()
-
 {
   Serial.begin(9600);
   stepper.setSpeed(rolePerMinute);
+    pinMode(7, INPUT);
 }
 
+bool switchOn()
+{
+  int switchState = digitalRead(2);
+  if (switchState != currentState) {
+    currentState = switchState;
+    if (currentState == 1) {
+      Serial.println("Switch in on state");
+    } else {
+      Serial.println("Switch in off state");
+    }
+  }
+  if (currentState == 1) {
+      return true;
+    } else {
+      return false;
+    }
+}
 
 void loop()
 {
@@ -23,12 +41,14 @@ void loop()
   int i = 0;
   int target_steps = stepsPerRevolution / 2;
   while (i < target_steps) {
-    if (clockwise) {
-      stepper.step(-1);
-    } else {
-      stepper.step(1);
+    if (switchOn()) {
+      if (clockwise) {
+        stepper.step(-1);
+      } else {
+        stepper.step(1);
+      }
+      i++;
     }
-    i++;
   }
 
   Serial.println("Reached target_steps");
