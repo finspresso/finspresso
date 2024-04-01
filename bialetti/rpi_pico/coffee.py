1fa97a1a483e94c611662558bd9376f86c0c46a3
finspresso <coder@finspresso.com>
2024-04-01 17:13:40 +0200

    Fix coffee.py

--- bialetti/rpi_pico/coffee.py
+++ bialetti/rpi_pico/coffee.py
@@ -4,16 +4,6 @@ import utime
 import time
 
 
-# adc = ADC(Pin(26))
-analogReadResolution = 65535
-beta = 3950
-TReference = 298.0
-KelvinOffset = 273.0
-adc_ponti = ADC(28)
-pwm = PWM(Pin(22))
-pwm.freq(1000)
-
-
 def kelvinToCentigradeClecius(tempK):
     return tempK - KelvinOffset
 
@@ -27,28 +17,47 @@ def readTemperature():
 
 
 def goToPosition(targetPosition, currentPosition):
-    # Move to zero position smoothly
+    print("targetPosition: ", targetPosition)
+    print("currentPosition: ", currentPosition)
     if currentPosition > targetPosition:
         while currentPosition > targetPosition:
             pwm.duty_u16(currentPosition - 1)
-            utime.sleep_ms(2)
+            utime.sleep_us(10)
             currentPosition = currentPosition - 1
     else:
         while currentPosition < targetPosition:
             pwm.duty_u16(currentPosition + 1)
-            utime.sleep_ms(2)
+            utime.sleep_us(10)
             currentPosition = currentPosition + 1
 
 
+# adc = ADC(Pin(26))
+analogReadResolution = 65535
+beta = 3950
+TReference = 298.0
+KelvinOffset = 273.0
+adcPonti = ADC(28)
+pwm = PWM(Pin(22))
+pwm.freq(1000)
 led = Pin("LED", Pin.OUT)
 led.on()
-while True:
-    reading = adc_ponti.read_u16()
-    refVoltage = 3.3
-    resolution = 2 ** 16 - 1
+refVoltage = 3.3
+resolution = 2 ** 16 - 1
 
-    voltage = (reading * refVoltage) / resolution
-    print("Voltage: ", voltage)
-    print("ADC: ", reading)
-    pwm.duty_u16(reading)
-    utime.sleep(0.2)
+reading = adcPonti.read_u16()
+voltage = (reading * refVoltage) / resolution
+print("Testreading:")
+print("Voltage: ", voltage)
+print("ADC: ", reading)
+currentPosition = int(resolution / 2)
+pwm.duty_u16(30000)
+# Loop to gradually change brightness (optional)
+# for duty_cycle in range(0, 65536, 50):
+#     pwm.duty_u16(duty_cycle)
+#     print("duty_cycle: ", duty_cycle)
+#     time.sleep_ms(100)  # Adjust delay for desired speed of change
+pwm.deinit()
+# while True:
+#     targetPosition = adcPonti.read_u16()
+#     goToPosition(targetPosition, currentPosition)
+#     currentPosition = targetPosition
