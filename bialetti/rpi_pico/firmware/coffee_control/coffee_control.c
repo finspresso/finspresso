fa1751dfc69ed181825c20658d99982beb6a7375
finspresso <coder@finspresso.com>
2024-05-20 16:08:56 +0200

    Output PWM levels based on pontetiometer input

--- bialetti/rpi_pico/firmware/coffee_control/coffee_control.c
+++ bialetti/rpi_pico/firmware/coffee_control/coffee_control.c
@@ -9,6 +9,34 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 #include "pico/cyw43_arch.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+
+const uint OUTPUT_PIN = 2;
+const uint MEASURE_PIN = 5;
+
+float measure_duty_cycle(uint gpio) {
+    // Only the PWM B pins can be used as inputs.
+    assert(pwm_gpio_to_channel(gpio) == PWM_CHAN_B);
+    uint slice_num = pwm_gpio_to_slice_num(gpio);
+
+    // Count once for every 100 cycles the PWM B input is high
+    pwm_config cfg = pwm_get_default_config();
+    pwm_config_set_clkdiv_mode(&cfg, PWM_DIV_B_HIGH);
+    pwm_config_set_clkdiv(&cfg, 100);
+    pwm_init(slice_num, &cfg, false);
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+
+    pwm_set_enabled(slice_num, true);
+    sleep_ms(10);
+    pwm_set_enabled(slice_num, false);
+    float counting_rate = clock_get_hz(clk_sys) / 100;
+    float max_possible_count = counting_rate * 0.01;
+    return pwm_get_counter(slice_num) / max_possible_count;
+}
+
+
+
 
 int main() {
     stdio_init_all();
@@ -25,9 +53,17 @@ int main() {
     // Select ADC input 0 (GPIO26)
     adc_select_input(0);
 
+    // Configure PWM slice and set it running
+    const uint count_top = 1000;
+    pwm_config cfg = pwm_get_default_config();
+    pwm_config_set_wrap(&cfg, count_top);
+    pwm_init(pwm_gpio_to_slice_num(OUTPUT_PIN), &cfg, true);
+    gpio_set_function(OUTPUT_PIN, GPIO_FUNC_PWM);
+
+    float ref_voltage = 3.3f; // [V]
     while (1) {
         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-        const float conversion_factor = 3.3f / (1 << 12);
+        const float conversion_factor = ref_voltage / (1 << 12);
         uint16_t result = adc_read();
         double voltage = result * conversion_factor;
         printf("Raw value: 0x%03x, voltage: %f V\n", result, voltage);
@@ -37,6 +73,11 @@ int main() {
         {
             cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         }
-        sleep_ms(500);
+        float output_duty_cycle = voltage / ref_voltage;
+        pwm_set_gpio_level(OUTPUT_PIN, (uint16_t) (output_duty_cycle * (count_top + 1)));
+        float measured_duty_cycle = measure_duty_cycle(MEASURE_PIN);
+        printf("Output duty cycle = %.1f%%, measured input duty cycle = %.1f%%\n",
+                output_duty_cycle * 100.f, measured_duty_cycle * 100.f);
+        sleep_ms(1000);
     }
 }
